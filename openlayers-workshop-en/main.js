b2045ee3eedee5ba0ce76e274473ed8b179277f0
kraet <156175162+KraetSpg@users.noreply.github.com>
2024-07-31 09:49:02 +0000

    added all functions i need

--- openlayers-workshop-en/main.js
+++ openlayers-workshop-en/main.js
@@ -8,18 +8,12 @@ import TileLayer from 'ol/layer/Tile';
 import Feature from 'ol/Feature.js';
 import { Icon, Style } from 'ol/style.js';
 import Point from 'ol/geom/Point.js';
-import MousePosition from 'ol/control/MousePosition.js';
 import Overlay from 'ol/Overlay.js';
 
-
-/**
- * Elements that make up the popup.
- */
-const container = document.getElementById('popup');
-const content = document.getElementById('popup-content');
-
+//PopUp when clicking
+const popup = document.getElementById("popup");
 const overlay = new Overlay({
-  element: container,
+  element: popup,
   autoPan: {
     animation: {
       duration: 250,
@@ -27,7 +21,7 @@ const overlay = new Overlay({
   },
 });
 
-
+//The map that is displayed (an OpenStreetLayerMap)
 const map = new Map({
   overlays: [overlay],
   target: 'map-container',
@@ -42,27 +36,26 @@ const map = new Map({
   }),
 });
 
-
 //used to store features
 const source = new VectorSource({
 });
 
+//Layer that gets added to the Map
 const featureLayer = new VectorLayer({
   source: source
 })
-
 map.addLayer(featureLayer)
 
 //Stay at same position with refresh
 map.addInteraction(new Link());
 
-let mousePos = new MousePosition();
+
 
 //How I can add an Popup on click (like creating a new Bikertreff)
 map.addEventListener("singleclick", (e) => {
+
   let coordinates = e.coordinate
 
-  content.innerHTML = '<p>You clicked here:</p><code></code>';
   overlay.setPosition(coordinates);
 
   let feature = new Feature({
@@ -84,3 +77,67 @@ map.addEventListener("singleclick", (e) => {
   source.addFeature(feature);
 })
 
+// #### BACKEND STUFF####
+function addFeature(coordinates, name) {
+
+  let feature = new Feature({
+    geometry: new Point(coordinates),
+    name: name,
+  })
+
+  const iconStyle = new Style({
+    image: new Icon({
+      anchor: [0.5, 46],
+      anchorXUnits: 'fraction',
+      anchorYUnits: 'pixels',
+      src: 'data/marker-black.svg',
+    }),
+  });
+
+  feature.setStyle(iconStyle);
+  source.addFeature(feature);
+}
+
+function getBikerMeetupsFromBackend() {
+  fetch("https://innolab.spengergasse.at/schueler/db/30/items/bikermeetups")
+    .then(response => response.json())
+    .then(bikertreffs => {
+      for (let i in bikertreffs.data) {
+        let bikertreff = bikertreffs.data[i];
+        addFeature([bikertreff.xValue, bikertreff.yValue], bikertreff.name)
+      }
+    })
+}
+
+
+//Take function to modular Scope to be able to call it in the HTML
+window.createMeetUp = createMeetUp;
+
+function createMeetUp() {
+  let name = document.getElementById('name').value;
+  let date = document.getElementById('startzeit').value;
+
+  let coordinates = overlay.getPosition();
+
+  fetch('https://innolab.spengergasse.at/schueler/db/30/items/bikermeetups', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: name,
+      xValue: coordinates[0],
+      yValue: coordinates[1]
+    })
+  }).then(response => response.json())
+    .then(json => console.log(json))
+}
+
+//Show Info on Hover over Feature
+map.on('pointermove', (event) => {
+  map.forEachFeatureAtPixel(event.pixel, (feature) => {
+    alert(feature.values_.name)
+  })
+})
+
+getBikerMeetupsFromBackend();
